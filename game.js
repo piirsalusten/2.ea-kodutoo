7e17edf85eab9b4171de315e545629ec55c97f91
Sten Piirsalu <piirsalusten@user.noreply.github.com>
2018-05-22 22:58:06 +0300

    3lehte olemas, vaja saada nime lisamine korda ja seejarel endgame nuppu vajutades localstoragei lisamine(see nupp voiks mangu ka lopetada)

--- game.js
+++ game.js
@@ -1,22 +1,24 @@
+//Globaalsed muutujad
 let guessedWords = 0
 let score = 0
-let timer = 0
 let mistakes = 0
+const MAXMISTAKES = 10
 let playerName
-const MAINAPP = function () {
-  if (MAINAPP.instance) {
-    return MAINAPP.instance
-  }
-  MAINAPP.instance = this
 
-  this.routes = MAINAPP.routes
+/* ONE PAGE APPLICATION */
+const MainApp = function () {
+  if (MainApp.instance) {
+    return MainApp.instance
+  }
+  MainApp.instance = this
+
+  this.routes = MainApp.routes
   this.currentRoute = null
 
   this.init()
 }
 
-
-
+/* TYPER */
 const TYPER = function () {
   if (TYPER.instance_) {
     return TYPER.instance_
@@ -35,37 +37,45 @@ const TYPER = function () {
 
   this.score = 0
   this.mistakes = 0
-  this.timer = 0
-
 
   this.init()
 }
 
 window.TYPER = TYPER
 
-MAINAPP.routes = {
-  'home-view': {
+MainApp.routes = {
+  'home': {
     'render': function () {
       console.log('>>>> Home')
+      document.getElementById('playerName')
+      .addEventListener('submit', saveName)
+      console.log(playerName)
     }
   },
-  'app-view': {
+  'game': {
     'render': function () {
       console.log('>>>> App')
-      const typer = new TYPER()
-      window.typer = typer
+      document.getElementById("startGame")
+      .addEventListener('click', startGame)
+      document.getElementById("endGame")
+      .addEventListener('click', endGame)
+    }
+  },
+  'scores': {
+    'render': function () {
+      console.log('>>>> scores')
     }
   }
 }
 
-MAINAPP.prototype = {
+MainApp.prototype = {
   init: function () {
     console.log('Rakendus läks tööle')
 
     window.addEventListener('hashchange', this.routeChange.bind(this))
 
     if (!window.location.hash) {
-      window.location.hash = 'home-view'
+      window.location.hash = 'home'
     } else {
       this.routeChange()
     }
@@ -84,13 +94,13 @@ MAINAPP.prototype = {
 
   updateMenu: function () {
     // http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
-    document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '')
-    document.querySelector('.' + this.currentRoute).className += ' active-menu'
+    document.querySelector('.active').className = document.querySelector('.active').className.replace('active', '')
+    document.querySelector('.' + this.currentRoute).className += ' active'
   }
 
 }// Main
 
-TYPER.prototype = { 
+TYPER.prototype = {
   init: function () {
     this.canvas = document.getElementsByTagName('canvas')[0]
     this.ctx = this.canvas.getContext('2d')
@@ -104,6 +114,7 @@ TYPER.prototype = {
     this.loadWords()
   },
 
+  //Toob failist sõnad
   loadWords: function () {
     const xmlhttp = new XMLHttpRequest()
 
@@ -134,10 +145,10 @@ TYPER.prototype = {
     const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
     const wordFromArray = this.words[generatedWordLength][randomIndex]
 
-    wordCounter = this.guessedWords
+    this.word = new Word(wordFromArray, this.canvas, this.ctx)
     score = this.score
     mistakes = this.mistakes
-    this.word = new Word(wordFromArray, this.canvas, this.ctx)
+    guessedWords = this.guessedWords
   },
 
   keyPressed: function (event) {
@@ -150,38 +161,29 @@ TYPER.prototype = {
 
       if (this.word.left.length === 0) {
         this.guessedWords += 1
-        //endTime = window.performance.now()
-        //console.log(endTime)
-        /*if (endTime - startTime < 1000) {
-          this.playerScore += 45
-        }
-        if (endTime - startTime < 1500) {
-          this.playerScore += 35
-        }
-        if (endTime -startTime < 2500) {
-          this.playerScore += 15
-        }
-        else{
-          this.playerScore -= 15
-        }*/
 
         this.generateWord()
       }
 
       this.word.Draw()
-    }
-    else {
-      this.mistakes += 1
-      if (this.score >= 100){
-        this.score -= 100
-      }
-      else{
-        this.score = 0
+      this.word.Result()
+    } else{
+      if(this.mistakes < 10){
+        this.mistakes += 1
+        if(this.score >= 50) {
+          this.score -= 50
+        }
+        if (this.score < 50){
+          this.score = 0
+        }
+      } else {
+        this.word.Gameover()
       }
     }
   }
 }
 
+/* WORD */
 const Word = function (word, canvas, ctx) {
   this.word = word
   this.left = this.word
@@ -191,20 +193,29 @@ const Word = function (word, canvas, ctx) {
 
 Word.prototype = {
   Draw: function () {
-	this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
     this.ctx.textAlign = 'center'
     this.ctx.font = '140px Courier'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
+  },
 
-	  this.ctx.textAlign = 'left'
+  Result: function () {
+    this.ctx.textAlign = 'left'
     this.ctx.font = '50px Courier'
     this.ctx.fillText("Guessed words: ", 30, 70)
-    this.ctx.fillText(wordCounter, 450, 70)
+    this.ctx.fillText(guessedWords, 450, 70)
     this.ctx.fillText("Score: ", 30, 100)
     this.ctx.fillText(score, 200, 100)
     this.ctx.fillText("mistakes: ", 30, 130)
     this.ctx.fillText(mistakes, 290, 130)
+  },
+
+  Gameover: function () {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.textAlign = 'center'
+    this.ctx.font = '280px courier'
+    this.ctx.fillText("Game Over!", this.canvas.width / 2, this.canvas.height / 2)
   },
 
   removeFirstLetter: function () {
@@ -212,31 +223,8 @@ Word.prototype = {
   }
 }
 
-
-function autosave () {
-// after typing init autosave
-
-  const doneTypingInterval = 2500
-
-  if (timer) { clearTimeout(timer) }
-  timer = window.setTimeout(function () {
-    // TODO check if really changed
-    saveLocal()
-    console.log('autosave')
-  }, doneTypingInterval)
-}
-
-function saveLocal () {
-  console.log(window.app)
-  localStorage.setItem('player', JSON.stringify(playerName))
-}
-
-function endGame () {
-  console.log("game over!")
-
-
-}
-
+/* HELPERS */
+// võtab sõna pikkuse
 function structureArrayByWordLength (words) {
   let tempArray = []
 
@@ -246,22 +234,32 @@ function structureArrayByWordLength (words) {
 
     tempArray[wordLength].push(words[i])
   }
-  
 
   return tempArray
 }
 
+function startGame () {
+  const typer = new TYPER()
+  window.typer = typer
+}
 
-// kui leht laetud käivitan app'i
+//https://medium.com/codingthesmartway-com-blog/pure-javascript-building-a-real-world-application-from-scratch-5213591cfcd6
+function endGame () {
+  console.log("Game Over!")
+  let nameScore = {
+    name: playerName,
+    playerScore: score
+  }
+
+  localStorage.setItem('tulemus:', JSON.stringify(nameScore))
+}
+
+function saveName () {
+  playerName = document.getElementById('playerName').value
+  console.log(playerName)
+}
+
 window.onload = function () {
-  const app = new MAINAPP()
-  window.app = app
- 
-  document
-    .querySelector('#play-game')
-    .addEventListener('click', saveLocal)
-
-    playerName = document.querySelector('#player-name')
-
-  window.addEventListener('keypress', autosave)
+  const mainapp = new MainApp()
+  window.mainapp = mainapp
 }
